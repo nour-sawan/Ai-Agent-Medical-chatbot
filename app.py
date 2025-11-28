c6c3c63811125870df8d5e555a3f576f5bf0c11e
Nour Sawan <nsawan728@gmail.com>
2025-11-28 04:42:28 +0300

    Fix RetrievalQA import path

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 
